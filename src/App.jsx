8b14ec79a417f806fa805ed28dd307d8311ed13e
chocopie777 <egoran99@gmail.com>
2023-11-16 14:53:12 +0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import {useState} from "react";
+import TodoList from "./components/TodoList";
+import InputField from "./components/InputField";
+import {useDispatch} from "react-redux";
+import {addTodo} from "./store/todoSlice";
+
+function App() {
+    const [text, setText] = useState('');
+    const dispatch = useDispatch();
+
+    const addTask = () => {
+        dispatch(addTodo({text}))
+        setText('');
+    }
+
+    return (
+        <div className="App">
+            <InputField text={text} handleInput={setText} handleSubmit={addTask}/>
+            <TodoList/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/store/todoSlice.js
@@ -0,0 +1,27 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState: {
+    todos: [],
+  },
+  reducers: {
+    addTodo(state, action) {
+      state.todos.push({
+        id: new Date().toISOString(),
+        text: action.payload.text,
+        completed: false,
+      })
+    },
+    removeTodo(state, action) {
+      state.todos = state.todos.filter(t => t.id !== action.payload.id);
+    },
+    toggleTodoComplete(state, action) {
+      const toggledTodo = state.todos.find(t => t.id === action.payload.id);
+      toggledTodo.completed = !toggledTodo.completed;
+    },
+  },
+})
+
+export const {addTodo, removeTodo, toggleTodoComplete} = todoSlice.actions;
+export const todoReducer = todoSlice.reducer;
